ac5f035b76048a4f88fbbd974f734987f79fde35
slikts <dabas@untu.ms>
2013-09-22 21:59:51 +0300

    poll for location change to focus

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-document.getElementById('player').getElementsByTagName('embed')[0].focus();
+var playerEmbed = document.getElementById('player').getElementsByTagName('embed')[0];
+function focusPlayerEmbed() {
+	playerEmbed.focus();
+}
+var href;
+// Poll for location changes
+setInterval(function() {
+	if (href === window.location.href) {
+		return;
+	}
+	href = window.location.href;
+	focusPlayerEmbed();
+}, 100);
